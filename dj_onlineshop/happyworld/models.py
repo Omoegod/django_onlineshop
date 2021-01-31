953b8ba6bf096a0430553c35176a4b10524f24b9
Vadzim <omoegod@icloud.com>
2021-01-31 21:40:56 +0300

    created reference part two

--- dj_onlineshop/happyworld/models.py
+++ dj_onlineshop/happyworld/models.py
@@ -9,7 +9,19 @@ class Author(models.Model):
         return self.name
 
 class Genre(models.Model):
-    name_genre = models.CharField("Name genre?", max_length=30)
+    name_genre = models.CharField("Name genre", max_length=30)
     
     def __str__(self):
-        return self.name_genre         
+        return self.name_genre
+        
+class Series(models.Model):
+    name_series = models.CharField("Name series", max_length=30)
+
+    def __str__(self):
+        return self.name_series
+
+class Publishing(models.Model):
+    name_publishing = models.CharField("Name publishing house", max_length=50)
+
+    def __str__(self):
+        return self.name_publishing
--- dj_onlineshop/proj/settings.py
+++ dj_onlineshop/proj/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'europe/Minsk'
+TIME_ZONE = 'Europe/Minsk'
 
 USE_I18N = True
 
